64a824b32d45f1765312c160b62304a63f41456d
CHiEF-fj <rtemosibaleinuku@gmail.com>
2023-06-23 15:54:03 +1200

    add images

--- src/app/pages/tabs/add/add.page.ts
+++ src/app/pages/tabs/add/add.page.ts
@@ -7,6 +7,7 @@ import { NavController } from '@ionic/angular';
   styleUrls: ['./add.page.scss'],
 })
 export class AddPage {
+  selectedImage: string = '';
   firstname: string = '';
   lastname: string = '';
   dob: string = '';
@@ -18,10 +19,31 @@ export class AddPage {
 
   constructor(private navCtrl: NavController) {}
 
+  onImageSelected(event: any) {
+    const file: File = event.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = (e: any) => {
+        this.selectedImage = e.target.result;
+      };
+      reader.readAsDataURL(file);
+    }
+  }
+  
   onSubmit() {
     // Handle form submission logic here
     // You can access the form values using the component properties
-    console.log('Form submitted:', this.firstname, this.lastname, this.dob, this.position, this.totalWins, this.totalMatches, this.jersey, this.club);
+    const inputData = {
+      firstname: this.firstname,
+      lastname: this.lastname,
+      dob: this.dob,
+      position: this.position,
+      totalWins: this.totalWins,
+      totalMatches: this.totalMatches,
+      jersey: this.jersey,
+      club: this.club,
+    };
+
 
     // Reset the form fields after submission if needed
     this.firstname = '';
@@ -34,6 +56,6 @@ export class AddPage {
     this.club = '';
 
     // Optionally, navigate to another page after form submission
-    this.navCtrl.navigateForward('/team-list');
+    this.navCtrl.navigateForward('/team-list', { state: inputData});
   }
 }
